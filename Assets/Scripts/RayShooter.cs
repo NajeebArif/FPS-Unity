d17a2f1b48e1c4809d4cb4125b7a8efffff07e79
Najeeb Arif <NajeebArif786@outlook.com>
2021-03-23 14:35:47 +0530

    Adding the first commit

---
+++ Assets/Scripts/RayShooter.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Camera _camera;
+
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnGUI()
+    {
+        int size = 12;
+        float posX = _camera.pixelWidth / 2 - size / 4;
+        float posY = _camera.pixelHeight / 2 - size / 2;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+            Ray ray = _camera.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+                StartCoroutine(sphereIndicator(hit.point));
+        }
+    }
+
+    private IEnumerator sphereIndicator(Vector3 pos)
+    {
+        GameObject bulletHoles = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        bulletHoles.transform.position = pos;
+        yield return new WaitForSeconds(1);
+        Destroy(bulletHoles);
+    }
+}
