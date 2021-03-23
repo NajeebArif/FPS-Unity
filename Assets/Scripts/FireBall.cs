58a3d98044f800a4ca8efa12ec04f82d12383d32
Najeeb Arif <NajeebArif786@outlook.com>
2021-03-23 18:31:37 +0530

    Adding fire capability to enemy

--- Assets/Scripts/FireBall.cs
+++ Assets/Scripts/FireBall.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FireBall : MonoBehaviour
 {
+    public float speed = 20.0f;
+    public int damage = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,14 @@ public class FireBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        PlayerCharacter playerCharacter = other.GetComponent<PlayerCharacter>();
+        if(playerCharacter != null) {
+            Debug.Log("Player Hit.");
+        }
+        Destroy(this.gameObject);
     }
 }
